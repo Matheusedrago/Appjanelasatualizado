55211a7430a3a725fce3a53b7c15871d675509b5
Matheusedrago <matheusdrago2018@gmail.com>
2024-05-09 20:04:58 -0300

    atualizado

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,17 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
